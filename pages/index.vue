20582d2a88d4cad5edd8630118049b4c569625ce
chihaoyo <chihaoyo@gmail.com>
2019-11-25 22:42:56 +0800

    Add url-parse; update gdoc.js; update index styles

--- pages/index.vue
+++ pages/index.vue
@@ -37,10 +37,13 @@ export default {
 @import '~assets/styles/resources';
 
 .page.index {
+  padding-bottom: 2rem;
+  background-color: #ccc;
   $nav-height: 46px;
   > nav {
     display: flex;
     align-items: center;
+    background-color: black;
     > .icon {
       height: $nav-height;
     }
@@ -49,7 +52,16 @@ export default {
       line-height: $line-height-single;
       margin: 0 0.5rem;
       padding: 0;
+      color: white;
     }
+  }
+  > .gdoc {
+    max-width: 36rem;
+    margin: 2rem auto;
+    padding: 1rem;
+    background-color: white;
+    border-radius: 0.25rem;
+    box-shadow: 0 8px 48px 0 rgba(black, .25);
   }
 }
 </style>
